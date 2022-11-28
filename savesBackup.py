f481c4fddf111fac62323e6c5556fa044284cdba
jimnorrain <67312973+jimnorrain@users.noreply.github.com>
2022-11-27 18:21:27 -0600

    Added 1st script

---
+++ savesBackup.py
@@ -0,0 +1,29 @@
+# Created by J. Rain
+# 11/2022
+
+import os
+import shutil
+import csv
+
+def main():
+
+    reader = csv.reader(open('steam_saves.csv', 'r'))
+    d = {}
+    for row in reader:
+        k, v = row
+        d[k] = v
+        
+    for k, v in d.items():
+    
+        sourceDir = k
+        print(sourceDir)
+        targetDir = d[k]
+        print(targetDir)
+
+        saveFiles = os.listdir(sourceDir)
+        
+        for files in saveFiles:
+            shutil.copy(os.path.join(sourceDir, files), targetDir)
+            print(f"{sourceDir}{files} moved successfully.")
+   
+main()
